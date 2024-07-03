8bc60f307d6e9bce00d3edaef40ab9608a5e9701
Crosser023 <134896406+Crosser023@users.noreply.github.com>
2024-07-02 22:37:32 -0400

    f

--- tiendaweb/urls.py
+++ tiendaweb/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='index'),  # Página de inicio
     path('registro/', views.registro_cliente, name='registro_cliente'),  # Página de registro
-    path('texto/', views.texto, name='texto'), #en proceso
+    #path('texto/', views.texto, name='texto'), #en proceso
 ]
